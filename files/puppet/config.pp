0f8cd283117eb5d7bf770e5d48dd1bc88eeffac9
Florent Peyraud <florent@tryphon.eu>
2010-04-09 21:16:21 +0200

    added tuner volume and frequency parameters and fm daemonized

--- files/puppet/config.pp
+++ files/puppet/config.pp
@@ -3,3 +3,6 @@ $network_static_address="192.168.1.2"
 $network_static_netmask="255.255.255.0"
 $network_static_gateway="192.168.1.1"
 $network_static_dns1="192.168.1.1"
+
+$input_frequency="107.7"
+$input_volume="100"
--- files/puppet/manifests/site.pp
+++ files/puppet/manifests/site.pp
@@ -46,6 +46,22 @@ exec { "create-streamcontrol-db":
   tag => boot
 }
 
+file { "/var/etc/fm/":
+  ensure => directory,
+  tag => boot
+}
+
+file { "/var/etc/fm/fm.conf":
+  content => template("/etc/puppet/templates/fm.conf"),
+  notify => Service[fm],
+  tag => boot
+}
+
+service { fm:
+  ensure => running,
+  hasrestart => true
+}
+
 file { "/var/etc/darkice/":
   ensure => directory,
   tag => boot
