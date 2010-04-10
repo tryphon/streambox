7f021a3c77798e4ac8792e10e9ed273025eb2006
Alban Peignier <alban.peignier@free.fr>
2010-04-10 23:18:39 +0200

    Integrate streamcontrol 0.4 with tuner_input attributes

--- files/puppet/config.pp
+++ files/puppet/config.pp
@@ -4,5 +4,6 @@ $network_static_netmask="255.255.255.0"
 $network_static_gateway="192.168.1.1"
 $network_static_dns1="192.168.1.1"
 
-$input_frequency="107.7"
-$input_volume="100"
+$alsa_input_gain="-3"
+$tuner_input_frequency="107.7"
+$tuner_input_volume="100"
