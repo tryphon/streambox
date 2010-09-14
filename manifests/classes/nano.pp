f517956d30052fc7472e0b5c91f9d33f2acec5da
Alban Peignier <alban@tryphon.eu>
2010-09-14 20:57:39 +0200

    Update puppet module to use box and puppet classes. Fixes #14

--- manifests/classes/nano.pp
+++
@@ -1,3 +0,0 @@
-class nano {
-  package { nano: }
-}
--- manifests/site.pp
+++ manifests/site.pp
@@ -7,36 +7,17 @@ import "box"
 $source_base="/tmp/puppet"
 
 $box_name="streambox"
+include box
+
 $amixerconf_mode="capture"
+include box::audio
 
-include release
-include users
-include network
-include network::interfaces
-
-include linux::kernel-2-6-30
-include syslog
-include smtp
-include nano
-include ssh
-
-include dbus::readonly
-include avahi
-
-include apt
-include apt::tryphon
-include puppet
-include sudo
-
-include alsa::common
-include alsa::oss # troubles with alsa & darkice 
-include tuner
-
-$darkice_user = "stream"
-include darkice::full
 include apache
 include apache::dnssd
 include streamcontrol
 
-include munin::readonly
-include munin-node::local
+$darkice_user="stream"
+include users
+include tuner
+include darkice::full
+
