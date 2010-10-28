c440de58e87f1072770d9238c9158eff02d6becf
Alban Peignier <alban@tryphon.eu>
2010-10-28 09:53:06 +0200

    Load syslog_logger before using it as config.logger

--- files/streamcontrol/production.rb
+++ files/streamcontrol/production.rb
@@ -8,6 +8,7 @@ config.cache_classes = true
 # config.threadsafe!
 
 # Use a different logger for distributed setups
+require 'syslog_logger'
 config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
