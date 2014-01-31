6e8205e4c8015e79d134bc40f435c470887859bd
Alban Peignier <alban@tryphon.eu>
2014-01-31 16:08:10 +0100

    Save box log when scenario failed. Retry when VM rollback failed

--- features/support/box.rb
+++ features/support/box.rb
@@ -1,9 +1,31 @@
 require 'vmbox'
+require 'open-uri'
+require 'fileutils'
 
 def current_box
   @current_box = VMBox.new("streambox")
 end
 
-After do
-  current_box.rollback
+After do |scenario|
+  if scenario.failed?
+    open("http://streambox.local/log.gz", "rb") do |read_file|
+      File.open("log/cucumber-syslog-#{Time.now.to_i}.gz", "wb") do |saved_file|
+        FileUtils.copy_stream read_file, saved_file
+      end
+    end
+  end
+
+  retry_count = 0
+  begin
+    current_box.rollback
+  rescue Timeout::Error => e
+    retry_count += 1
+    VMBox.logger.error "Rollback failed : #{e}"
+    if retry_count < 10
+      retry
+    else
+      VMBox.logger.error "Stop tests"
+      Cucumber.wants_to_quit = true
+    end
+  end
 end
