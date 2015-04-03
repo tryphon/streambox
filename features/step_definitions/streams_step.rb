b1e431b7661d40b9d64f7f9f0fcfcd3a68e32215
Alban Peignier <alban@tryphon.eu>
2015-04-03 11:47:52 +0200

    Fixes stream test spec. Refs #1223

--- features/step_definitions/streams_step.rb
+++ features/step_definitions/streams_step.rb
@@ -13,22 +13,24 @@ def stream_content_type(url)
     end
   end
   content_type
+rescue Exception => e
+  nil
 end
 
 Then /^an? ([^ ]*) stream should respond on "([^\"]*)"$/ do |type, url|
   expected_content_type = {
     "ogg" =>  "application/ogg",
     "mp3" => "audio/mpeg",
-    "aac" => "audio/aac",
+    "aac" => "audio/aacp",
   }[type]
 
   content_type = stream_content_type(URI.parse(url))
   retry_count = 3
-  while retry_count > 0 and content_type == "text/html"
+  while retry_count > 0 and (content_type.nil? or content_type == "text/html")
     retry_count -= 1
     sleep 3
     content_type = stream_content_type(URI.parse(url))
   end
 
-  content_type.should == content_type
+  expect(content_type).to eq(expected_content_type)
 end
